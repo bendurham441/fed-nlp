7c714ff504be3c6c76bcedb4d28204da6a9d315f
Ben Durham <bendurham441@gmail.com>
2023-11-14 18:13:30 -0500

    Moved tokenization/processing to its own file to allow for use in multiple notebooks

---
+++ utils.py
@@ -0,0 +1,24 @@
+
+# def test():
+    # print('test')
+
+
+# def other():
+    # print('thing')
+
+
+def process(raw_string, stemmer=None):
+    from nltk import word_tokenize
+    from nltk.corpus import stopwords
+
+    import string
+
+
+    final = str(raw_string).lower()
+    tokens = word_tokenize(final)
+    final = [word for word in tokens if word not in stopwords.words('english')]
+    if stemmer:
+        stemmed = [stemmer.stem(token) for token in final]
+        final = stemmed
+    final = [token for token in final if token not in string.punctuation]
+    return final
