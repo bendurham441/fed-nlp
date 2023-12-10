9b119df9d615b5f6bf14b7701878cdedd0822c9f
Ben Durham <bendurham441@gmail.com>
2023-12-09 20:05:08 -0500

    Added (default) option to remove numeric tokens for topic modelling

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 
-def process(raw_string, stemmer=None):
+def process(raw_string, stemmer=None, drop_numbers=True):
     from nltk import word_tokenize
     from nltk.corpus import stopwords
     import string
 
-
     final = str(raw_string).lower()
     tokens = word_tokenize(final)
     final = [word for word in tokens if word not in stopwords.words('english')]
+    if drop_numbers:
+        final = [token for token in final if not token.isnumeric()]
     if stemmer:
         stemmed = [stemmer.stem(token) for token in final]
         final = stemmed
