d6f4deab32c792c0e3831b4c669bb943d557f27d
Ben Durham <bendurham441@gmail.com>
2024-02-10 15:43:34 -0500

    Added ngram enhancement to utility function

--- utils.py
+++ utils.py
@@ -14,3 +14,32 @@ def process(raw_string, stemmer=None, drop_numbers=True, drop_punct=True):
         final = stemmed
     final = [token for token in final if token not in string.punctuation]
     return final
+
+
+def setup_enhance(bigrams_path, trigrams_path):
+    import os
+    bigrams = set()
+
+    with open(os.path.join('ngrams', 'bigrams')) as bg_file:
+        for bg in bg_file.readlines():
+            bigrams.add(bg.strip())
+
+    trigrams = set()
+
+    with open(os.path.join('ngrams', 'trigrams')) as tg_file:
+        for tf in tg_file.readlines():
+            trigrams.add(tf.strip())
+
+    return bigrams, trigrams
+
+def ngram_enhance(raw_string, bigrams, trigrams):
+    to_add = []
+    raw_string = str(raw_string)
+    for bg in bigrams:
+        if str(bg) in raw_string.lower():
+            to_add.append('_'.join(bg.split(' ')))
+    for tg in trigrams:
+        if str(tg) in raw_string.lower():
+            to_add.append('_'.join(tg.split(' ')))
+
+    return raw_string + ' ' + ' '.join(to_add)
